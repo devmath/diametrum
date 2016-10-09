5b3ddce943bb1926b8a25d8f08622fbfa6bb8091
mhernandes <midia.matheus@gmail.com>
2016-10-09 20:31:44 -0300

    add undo-search

--- js/search.js
+++ js/search.js
@@ -1,35 +1,10 @@
 var input = document.getElementById('search-entry'),
 	searchButton = document.getElementById('search'),
 	entry = '',
-	article = '';
-
-searchButton.addEventListener('click', function() {
-	entry = input.value;
-	searching(entry);
-});
-
-function searching(value) {	
-	article = document.getElementsByClassName('each-article');
-
-	if (verify(value)) {
-		console.log('true');
-	} else {
-		console.log('false');
-	}
-
-	for (var i = 0; i < article.length; i++) {
-		var that = article[i],
-			tags = that.getAttribute('tags');
-
-		if (tags.indexOf(value) !== -1) {
-			//
-		} else {
-			that.style.display = 'none';
-		}
-	}
-
-	return true;
-}
+	article = '',
+	articleSection = document.getElementById("articles"),
+	undoButton = document.getElementById("undo-search"),
+	alertSection = document.getElementById("alert");
 
 function verify(value) {
 	var counting_articles = 0;
@@ -49,3 +24,51 @@ function verify(value) {
 		return false;
 	}
 }
+
+function displayError() {
+	articles.style.display = 'none';
+	alertSection.style.display = 'block';
+	input.value = '';
+
+	return true;
+}
+
+function undoSearch() {
+	articles.style.display = '-o-flex';
+	articles.style.display = '-ms-flex';
+	articles.style.display = '-moz-flex';
+	articles.style.display = '-webkit-flex';
+	articles.style.display = 'flex';
+	alertSection.style.display = 'none';
+	return true;
+}
+
+function searching(value) {	
+	article = document.getElementsByClassName('each-article');
+
+	if (verify(value)) {
+		for (var i = 0; i < article.length; i++) {
+			var that = article[i],
+				tags = that.getAttribute('tags');
+
+			if (tags.indexOf(value) !== -1) {
+				//
+			} else {
+				that.style.display = 'none';
+			}
+		}
+	} else {
+		displayError();
+	}
+
+	return true;
+}
+
+searchButton.addEventListener('click', function() {
+	entry = input.value;
+	searching(entry);
+});
+
+undoButton.addEventListener('click', function() {
+	undoSearch();
+});
