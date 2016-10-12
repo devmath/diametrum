3dd5d66fb37cfb6fd5df7ffc742f658b3970ab7b
mhernandes <midia.matheus@gmail.com>
2016-10-11 22:21:35 -0300

    remove search

--- js/effect.js
+++
@@ -1,9 +0,0 @@
-/*window.addEventListener('scroll', function(e) {
-	for (var i = 0; i < each_article.length; i++) {
-		var content = each_article[i].querySelector('.content');
-
-		if (content.style.transform < 58) {
-			content.style.transform = 'translateY(-' + (window.pageYOffset / 2) + 'px)';
-		}
-	}
-}, false);*/
